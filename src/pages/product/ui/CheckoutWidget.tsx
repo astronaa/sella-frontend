24a53c3b0cf3a98caafc8118c77bb192f797dbb2
Xelson <xelson1337@gmail.com>
2024-07-13 19:51:53 +0300

    Add edit product button for product-checkout page

--- src/pages/product/ui/CheckoutWidget.tsx
+++ src/pages/product/ui/CheckoutWidget.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import { useProductStrictContext } from "~/entities/product";
 import { useUserGetQuery } from "~/entities/user";
 import { OrderCreateBaseCard } from "~/features/order/create";
+import { ProductManageDialog } from "~/features/product/manage";
 import { useRegisterFlow } from "~/features/register";
 import { objToSearchParams } from "~/shared/lib/search-params";
 import { Button } from "~/shared/ui/kit/button";
+import { Skeleton } from "~/shared/ui/kit/skeleton";
 
 export function CheckoutWidget() {
 	const product = useProductStrictContext();
@@ -19,43 +21,59 @@ export function CheckoutWidget() {
 			product={product}
 		>
 			{({ disabled, ...method }) => (
-				<div className="flex flex-col gap-4">
-					<Link
-						className='w-full'
-						href={`${product.id}/checkout?${objToSearchParams({ tab: 'order-actions', ...method })}`}
-						onClick={e => {
-							if(!user) {
-								e.preventDefault();
-								e.stopPropagation();
-
-								startFlow(true);
-							}
-						}}
-					>
-						<Button
-							className='w-full'
-							variant="solid"
-							disabled={disabled}
-						>
-							Checkout
-						</Button>
-					</Link>
-
-					{!!user && (
-						<Link
-							className='w-full'
-							href={`${product.id}/checkout?${objToSearchParams({ tab: 'chat', ...method })}`}
-						>
+				disabled === true ? (
+					<ProductManageDialog
+						product={product}
+						triggerElement={
 							<Button
 								className='w-full'
-								colorPalette="gray"
-								disabled={disabled}
+								variant="solid"
 							>
-								Chat with a seller
+								Edit Product
 							</Button>
-						</Link>
-					)}
-				</div>
+						}
+					/>
+				) : (
+					<div className="flex flex-col gap-4">
+						<Skeleton asChild loading={disabled === undefined}>
+							<Link
+								className='w-full'
+								href={`${product.id}/checkout?${objToSearchParams({ tab: 'order-actions', ...method })}`}
+								onClick={e => {
+									if (!user) {
+										e.preventDefault();
+										e.stopPropagation();
+
+										startFlow(true);
+									}
+								}}
+							>
+								<Button
+									className='w-full'
+									variant="solid"
+									disabled={disabled}
+								>
+									Checkout
+								</Button>
+							</Link>
+						</Skeleton>
+
+						{!!user && (
+							<Link
+								className='w-full'
+								href={`${product.id}/checkout?${objToSearchParams({ tab: 'chat', ...method })}`}
+							>
+								<Button
+									className='w-full'
+									colorPalette="gray"
+									disabled={disabled}
+								>
+									Chat with a seller
+								</Button>
+							</Link>
+						)}
+					</div>
+				)
 			)}
 		</OrderCreateBaseCard>
 	)
