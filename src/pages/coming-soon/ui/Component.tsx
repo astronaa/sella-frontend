38591cd49fc980f725875f89457baac9cda852b7
ural1493 <ural1493@gmail.com>
2024-11-21 22:01:55 +0300

    fix line height and desktop scroll

--- src/pages/coming-soon/ui/Component.tsx
+++ src/pages/coming-soon/ui/Component.tsx
@@ -5,37 +5,40 @@ import { GradientBackground } from '~/shared/ui/gradient-background';
 
 export function Component() {
 	return (
-		<div className="flex flex-col items-center min-h-screen relative text-center font-tt">
+		<div className=" lg:h-screen lg:overflow-hidden">
+			<div className="flex flex-col items-center min-h-screen relative text-center font-tt bg-black">
+				<GradientBackground className='z-1 lg:fixed lg:bottom-0' />
 
-			<Logo className="mt-6 mb-24"/>
+				<Logo className="mt-6 mb-24 relative z-10"/>
 
-			<div>
-				<p
-					style={{
-						fontWeight: 600,
-						letterSpacing: '3px',
-					}}
-					className='text-xl max-md:text-sm uppercase text-black-74 text-center'
-				>the marketplace of tomorrow</p>
+				<div className='relative z-10'>
+					<p
+						style={{
+							fontWeight: 600,
+							letterSpacing: '3px',
+						}}
+						className='text-xl max-md:text-sm uppercase text-black-74 text-center'
+					>the marketplace of tomorrow</p>
 
-				<p
-					className="text-9xl max-md:text-7xl leading-normal"
-					style={{
-						fontWeight: 600,
-						textAlign: 'center',
-						background: 'linear-gradient(180deg, #EC9515 0%, #FFDD00 100%)',
-						backgroundClip: 'text',
-						color: 'transparent'
-					}}
-				>Coming Soon</p>
+					<p
+						className="text-9xl max-md:text-7xl leading-normal max-md:leading-normal max-sm:leading-tight"
+						style={{
+							fontWeight: 600,
+							textAlign: 'center',
+							background: 'linear-gradient(180deg, #EC9515 0%, #FFDD00 100%)',
+							backgroundClip: 'text',
+							color: 'transparent'
+						}}
+					>Coming Soon</p>
+				</div>
+
+
+				<div className="w-full overflow-hidden flex justify-center items-center">
+					<Image className='rotate-[-15deg] max-md:scale-125 max-md:mt-[4rem]' src={ImgSrc} alt="coming soon" width={960} height={960}/>
+				</div>
+
 			</div>
-
-
-			<div className="w-full overflow-hidden flex justify-center items-center">
-				<Image className='rotate-[-15deg] max-md:scale-125 max-md:mt-[4rem]' src={ImgSrc} alt="coming soon" width={960} height={960}/>
-			</div>
-
-			<GradientBackground className='z-[-1]' />
 		</div>
+
 	)
 }
