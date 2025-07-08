a388af9d96037c692a914aa36b945d4bc4618743
shift <zivunho@gmail.com>
2025-07-08 15:56:10 +0200

    Update InputAddon.tsx

--- src/entities/store/ui/InputAddon.tsx
+++ src/entities/store/ui/InputAddon.tsx
@@ -18,7 +18,7 @@ function Addon({ className, ...props }: AddonProps) {
 	return (
 		<span
 			{...props}
-			className={cn('flex items-center absolute top-0 h-full ps-[1rem] text-black-60 cursor-default', className)}
+			className={cn('flex items-center absolute top-0 h-full ps-[1rem] text-black-40 cursor-default', className)}
 		>
 			sella.store/
 		</span>
