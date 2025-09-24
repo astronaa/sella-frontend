f52aab19203ee4f9422a4aa5c33326d8ba733e80
MickaelNeves <nevesmickael8@gmail.com>
2025-09-24 12:37:56 +0100

    fix: rainbow kit issues

--- src/shared/config/rainbow-kit.ts
+++ src/shared/config/rainbow-kit.ts
@@ -1,25 +1,62 @@
-'use client';
+"use client";
 
-import {
-	mainnet,
-	polygon,
-	sepolia
-} from 'wagmi/chains';
+import { mainnet, polygon, sepolia } from "wagmi/chains";
+import { http } from "wagmi";
 
-import { darkTheme, getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { resolvedTwConfig } from '../lib/resolved-tw-config';
+import { darkTheme, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { resolvedTwConfig } from "../lib/resolved-tw-config";
+
+const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY;
+
+const customSepolia = {
+  ...sepolia,
+  rpcUrls: {
+    default: {
+      http: [
+        ...(infuraApiKey
+          ? [`https://sepolia.infura.io/v3/${infuraApiKey}`]
+          : []),
+        "https://sepolia.gateway.tenderly.co",
+        "https://ethereum-sepolia-rpc.publicnode.com",
+      ],
+    },
+    public: {
+      http: [
+        ...(infuraApiKey
+          ? [`https://sepolia.infura.io/v3/${infuraApiKey}`]
+          : []),
+        "https://sepolia.gateway.tenderly.co",
+        "https://ethereum-sepolia-rpc.publicnode.com",
+      ],
+    },
+  },
+};
 
 export const wagmiConfig = getDefaultConfig({
-	appName: 'My RainbowKit App',
-	projectId: 'YOUR_PROJECT_ID',
-	chains: [mainnet, polygon, sepolia],
-	ssr: true,
+  appName: process.env.NEXT_PUBLIC_RAINBOWKIT_APP_NAME!,
+  projectId: process.env.NEXT_PUBLIC_RAINBOWKIT_PROJECT_ID!,
+  chains: [mainnet, polygon, customSepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [polygon.id]: http(),
+    [sepolia.id]: http(
+      infuraApiKey
+        ? `https://sepolia.infura.io/v3/${infuraApiKey}`
+        : "https://sepolia.gateway.tenderly.co",
+      {
+        timeout: 10_000,
+        retryCount: 3,
+        retryDelay: 1000,
+      }
+    ),
+  },
+  ssr: true,
 });
 
 const { colors } = resolvedTwConfig.theme;
 
 export const rainbowKitTheme = darkTheme({
-	accentColor: colors['accent-100'],
-	accentColorForeground: 'black',
-	overlayBlur: 'large'
+  accentColor: colors["accent-100"],
+  accentColorForeground: "black",
+  overlayBlur: "large",
 });
