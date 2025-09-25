7db60661f69e9ff938787f5698b9c3b24bcaacd9
MickaelNeves <nevesmickael8@gmail.com>
2025-09-25 08:30:44 +0100

    fix: test slow lcp

--- src/shared/assets/fonts/manrope/index.ts
+++ src/shared/assets/fonts/manrope/index.ts
@@ -3,5 +3,7 @@ import localFont from 'next/font/local'
 export const fontManrope = localFont({
 	src: './font.ttf',
 	display: 'swap',
-	variable: '--font-manrope'
+	variable: '--font-manrope',
+	fallback: ['system-ui', 'arial'],
+	preload: true
 })
