885afffe196272265259c808152193146dd12497
MickaelNeves <nevesmickael8@gmail.com>
2025-09-29 13:55:51 +0100

    fix: images

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ const baseUrlMediaPattern = new URL(
 const nextConfig = {
   staticPageGenerationTimeout: 600,
   images: {
+    deviceSizes: [320, 420, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384, 512, 640, 768, 1024],
     remotePatterns: [
       {
         hostname: baseUrlMediaPattern.hostname,
