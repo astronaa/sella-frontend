1e17779f323805f21698d6e609d1d962f1e7c2cc
MickaelNeves <nevesmickael8@gmail.com>
2025-09-29 14:33:12 +0100

    fix: build

--- src/pages/home/ui/Component.tsx
+++ src/pages/home/ui/Component.tsx
@@ -1,19 +1,19 @@
 import { Hero } from "./Hero";
-import {ExploreMarketPlace} from "./ExploreMarketplace";
+import { ExploreMarketPlace } from "./ExploreMarketplace";
 import { EscrowBuyerSeller } from "./EscrowBuyerSeller";
 import { SellaFeautes } from "./SellaFeatures";
 import { CreateStore } from "./CreateStore";
-import { Roadmap } from "./Roadmap";
+/* import { Roadmap } from "./Roadmap"; */
 
 export function Component() {
-	return (
-		<div className=''>
-			<Hero />
-			<ExploreMarketPlace />
-			<EscrowBuyerSeller />
-			<SellaFeautes />
-			<CreateStore />
-			{/*<Roadmap />*/}
-		</div>
-	);
+  return (
+    <div className="">
+      <Hero />
+      <ExploreMarketPlace />
+      <EscrowBuyerSeller />
+      <SellaFeautes />
+      <CreateStore />
+      {/*<Roadmap />*/}
+    </div>
+  );
 }
