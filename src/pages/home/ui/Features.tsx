b0a1a065b63985cd388cb86f37f43fef893f4f13
MickaelNeves <nevesmickael8@gmail.com>
2025-10-06 15:21:53 +0100

    fix: grammar feature

--- src/pages/home/ui/Features.tsx
+++ src/pages/home/ui/Features.tsx
@@ -2,38 +2,31 @@ import { PropsWithChildren } from "react";
 import { Icons } from "~/shared/ui/icons";
 
 export function FeaturesList() {
-	return (
-		<div className="flex flex-col gap-[1rem] max-w-[28.75rem]">
-			<Feature label="Rocket Start">
-				Launch storefront in 30 seconds
-			</Feature>
-			<Feature label="Sell Anything">
-				Digital or Physical, Good or Service
-			</Feature>
-			<Feature label="No KYC">Quick and easy</Feature>
-			<Feature label="100% Free">No hidden fees</Feature>
-			<Feature label="Robust Escrow">
-				Buyer or seller, we got your back
-			</Feature>
-			{/*
+  return (
+    <div className="flex flex-col gap-[1rem] max-w-[28.75rem]">
+      <Feature label="Rocket Start">Launch storefront in 30 seconds</Feature>
+      <Feature label="Sell Anything">
+        Digital or Physical, Goods or Services
+      </Feature>
+      <Feature label="No KYC">Quick and easy</Feature>
+      <Feature label="100% Free">No hidden fees</Feature>
+      <Feature label="Robust Escrow">Buyer or seller, we got your back</Feature>
+      {/*
 			<Feature label="Powered by $SELLA">
 				Exclusive benefits & perks
 			</Feature>
 			*/}
-		</div>
-	);
+    </div>
+  );
 }
 
 function Feature({ label, children }: PropsWithChildren<{ label: string }>) {
-	return (
-		<div className="flex gap-[0.75rem] items-center min-w-0">
-			<Icons.CircleChecked 
-				className="text-accent-100 size-[1.25rem] max-md:size-[1.5rem] flex-shrink-0" 
-			/>
-			<p className="md:truncate text-black-60 flex md:gap-1 flex-col md:flex-row">
-				<span className="text-white">{label}</span>{" "}
-				<span>- {children}</span>
-			</p>
-		</div>
-	);
+  return (
+    <div className="flex gap-[0.75rem] items-center min-w-0">
+      <Icons.CircleChecked className="text-accent-100 size-[1.25rem] max-md:size-[1.5rem] flex-shrink-0" />
+      <p className="md:truncate text-black-60 flex md:gap-1 flex-col md:flex-row">
+        <span className="text-white">{label}</span> <span>- {children}</span>
+      </p>
+    </div>
+  );
 }
