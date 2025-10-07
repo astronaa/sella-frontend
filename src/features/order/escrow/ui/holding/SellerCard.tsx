7c371866217d187b39d0c3dd7a3d73277e2d7fcd
MickaelNeves <nevesmickael8@gmail.com>
2025-10-07 17:09:14 +0100

    fix: seller message

--- src/features/order/escrow/ui/holding/SellerCard.tsx
+++ src/features/order/escrow/ui/holding/SellerCard.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { OrderProp } from "~/entities/order";
 import { Price } from "~/shared/ui/price";
@@ -7,48 +7,52 @@ import { ActionCallbacks } from "../FlowCard";
 import { AutomaticEscrowTimer } from "./AutomaticEscrowTimer";
 import { ClaimPaymentControl } from "./ClaimPaymentControl";
 
-interface SellerHoldCardProps extends ActionCallbacks, OrderProp, EscrowCard.RootProps {
-	onRequestRefetch?: () => void
+interface SellerHoldCardProps
+  extends ActionCallbacks,
+    OrderProp,
+    EscrowCard.RootProps {
+  onRequestRefetch?: () => void;
 }
 
 export function SellerHoldCard({
-	order, onActionFulfilled, onActionRejected, onRequestRefetch, ...props
+  order,
+  onActionFulfilled,
+  onActionRejected,
+  onRequestRefetch,
+  ...props
 }: SellerHoldCardProps) {
-	const { holdEndingAt } = order.transaction;
-	const holdHours = order.transaction.holdPeriod * 24;
+  const { holdEndingAt } = order.transaction;
+  const holdHours = order.transaction.holdPeriod * 24;
 
-	return (
-		<EscrowCard.Root {...props}>
-			<EscrowCard.Title>Escrow</EscrowCard.Title>
-			<EscrowCard.Wafer>
-				<EscrowCard.WaferHeading>
-					<span>Order has been paid</span>
-					<Price price={order.price} />
-				</EscrowCard.WaferHeading>
+  return (
+    <EscrowCard.Root {...props}>
+      <EscrowCard.Title>Escrow</EscrowCard.Title>
+      <EscrowCard.Wafer>
+        <EscrowCard.WaferHeading>
+          <span>Order has been paid</span>
+          <Price price={order.price} />
+        </EscrowCard.WaferHeading>
 
-				<EscrowCard.WaferContent>
-					The seller has been notified of your payment. Once they confirm fulfilment of your order, you will be notified.
-					Please review and release the funds within {holdHours} hours of receiving the item or service. If you do not release
-					the funds, they will be automatically released unless you initiate a dispute.
-				</EscrowCard.WaferContent>
-			</EscrowCard.Wafer>
+        <EscrowCard.WaferContent>
+          Payment received! The buyer&apos;s funds are secured in escrow.
+          Complete the order and you&apos;ll be able to claim your payment.
+          Funds automatically release to you in {holdHours} hours unless the
+          buyer opens a dispute.
+        </EscrowCard.WaferContent>
+      </EscrowCard.Wafer>
 
-			{holdEndingAt && (
-				<AutomaticEscrowTimer
-					holdEndingAt={holdEndingAt}
-				/>
-			)}
+      {holdEndingAt && <AutomaticEscrowTimer holdEndingAt={holdEndingAt} />}
 
-			<ClaimPaymentControl
-				order={order}
-				onActionFulfilled={onActionFulfilled}
-				onActionRejected={onActionRejected}
-				onRequestRefetch={onRequestRefetch}
-			/>
+      <ClaimPaymentControl
+        order={order}
+        onActionFulfilled={onActionFulfilled}
+        onActionRejected={onActionRejected}
+        onRequestRefetch={onRequestRefetch}
+      />
 
-			{/* <Button variant='subtle' colorPalette='red' size='xl'>
+      {/* <Button variant='subtle' colorPalette='red' size='xl'>
 				Open Dispute
 			</Button> */}
-		</EscrowCard.Root>
-	);
+    </EscrowCard.Root>
+  );
 }
