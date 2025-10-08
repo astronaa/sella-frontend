118e296474cf3bdcab5a3196dd94fd4c84804090
MickaelNeves <nevesmickael8@gmail.com>
2025-10-08 18:53:27 +0100

    fix: preserve line breaks

--- src/entities/store/ui/Card.tsx
+++ src/entities/store/ui/Card.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 
-'use client';
+"use client";
 
 import { HTMLArkProps, ark } from "@ark-ui/react";
 import { StoreProp } from "./Prop";
@@ -11,132 +11,133 @@ import { PreviewImage, PreviewImageProps } from "~/shared/ui/image";
 import { StoreProvider, useStoreStrictContext } from "../model/context";
 import { RatingRow } from "~/shared/ui/rating";
 
-export type RootProps = HTMLArkProps<'div'> & StoreProp;
+export type RootProps = HTMLArkProps<"div"> & StoreProp;
 
 export function Root({ store, className, ...props }: RootProps) {
-	return (
-		<StoreProvider value={store}>
-			<ark.div
-				{...props}
-				className={cn(
-					'border border-secondary p-[1rem] rounded-[1.25rem] flex gap-[2rem] items-start md:items-center max-w-[35rem]',
-					className
-				)}
-			/>
-		</StoreProvider>
-	);
+  return (
+    <StoreProvider value={store}>
+      <ark.div
+        {...props}
+        className={cn(
+          "border border-secondary p-[1rem] rounded-[1.25rem] flex gap-[2rem] items-start md:items-center max-w-[35rem]",
+          className
+        )}
+      />
+    </StoreProvider>
+  );
 }
 
-export function Image({ className, ...props }: Omit<PreviewImageProps, 'src' | 'alt'>) {
-	const { previewImage: imageUrl, name: title } = useStoreStrictContext();
+export function Image({
+  className,
+  ...props
+}: Omit<PreviewImageProps, "src" | "alt">) {
+  const { previewImage: imageUrl, name: title } = useStoreStrictContext();
 
-	return (
-		<PreviewImage
-			width={200}
-			height={200}
-			alt={`Image of ${title}`}
-			src={imageUrl ?? null}
-			{...props}
-			className={cn('rounded-full flex-shrink-0 shadow-sm', className)}
-		/>
-	);
+  return (
+    <PreviewImage
+      width={200}
+      height={200}
+      alt={`Image of ${title}`}
+      src={imageUrl ?? null}
+      {...props}
+      className={cn("rounded-full flex-shrink-0 shadow-sm", className)}
+    />
+  );
 }
 
-export function ImageDesktop({ className, ...props }: ComponentProps<typeof Image>) {
-	return (
-		<Image
-			{...props}
-			className={cn('max-xl:hidden', className)}
-		/>
-	);
+export function ImageDesktop({
+  className,
+  ...props
+}: ComponentProps<typeof Image>) {
+  return <Image {...props} className={cn("max-xl:hidden", className)} />;
 }
 
-export function ImageMobile({ className, ...props }: ComponentProps<typeof Image>) {
-	return (
-		<Image
-			{...props}
-			className={cn('size-[3rem] xl:hidden', className)}
-		/>
-	);
+export function ImageMobile({
+  className,
+  ...props
+}: ComponentProps<typeof Image>) {
+  return (
+    <Image {...props} className={cn("size-[3rem] xl:hidden", className)} />
+  );
 }
 
-export function Content({ className, ...props }: HTMLArkProps<'div'>) {
-	return (
-		<ark.div className={cn('flex flex-col gap-[1rem]', className)} {...props} />
-	)
+export function Content({ className, ...props }: HTMLArkProps<"div">) {
+  return (
+    <ark.div className={cn("flex flex-col gap-[1rem]", className)} {...props} />
+  );
 }
 
-export function Title({ className, children, ...props }: HTMLArkProps<'div'>) {
-	const { name: title, url: name, isVerified } = useStoreStrictContext();
+export function Title({ className, children, ...props }: HTMLArkProps<"div">) {
+  const { name: title, url: name, isVerified } = useStoreStrictContext();
 
-	return (
-		<ark.div
-			{...props}
-			className={cn(
-				'flex gap-[1rem] items-center text-[1.5rem]/[1.3] truncate',
-				'max-lg:text-[1.125rem]/[1.3]',
-				className
-			)}
-		>
-			{children}
+  return (
+    <ark.div
+      {...props}
+      className={cn(
+        "flex gap-[1rem] items-center text-[1.5rem]/[1.3] truncate",
+        "max-lg:text-[1.125rem]/[1.3]",
+        className
+      )}
+    >
+      {children}
 
-			<div className='flex flex-col gap-[0.25rem] w-full'>
-				<div className='flex items-center gap-[0.5rem] font-semibold font-manrope leading-[1.3]'>
-					<h1>{title}</h1>
-					{isVerified && <Icons.Verified className='text-accent-100 size-[0.85em]' />}
-				</div>
+      <div className="flex flex-col gap-[0.25rem] w-full">
+        <div className="flex items-center gap-[0.5rem] font-semibold font-manrope leading-[1.3]">
+          <h1>{title}</h1>
+          {isVerified && (
+            <Icons.Verified className="text-accent-100 size-[0.85em]" />
+          )}
+        </div>
 
-				<span className='font-semibold text-black-40 text-[1rem]'>
-					{name}
-				</span>
-			</div>
-		</ark.div>
-	);
+        <span className="font-semibold text-black-40 text-[1rem]">{name}</span>
+      </div>
+    </ark.div>
+  );
 }
 
-export function Description({ className, ...props }: HTMLArkProps<'p'>) {
-	const { description } = useStoreStrictContext();
+export function Description({ className, ...props }: HTMLArkProps<"p">) {
+  const { description } = useStoreStrictContext();
 
-	return (
-		<ark.p className={cn('text-black-60 leading-[1.3]', className)} {...props}>
-			{description}
-		</ark.p>
-	);
+  return (
+    <ark.p
+      className={cn(
+        "text-black-60 leading-[1.3] whitespace-pre-line",
+        className
+      )}
+      {...props}
+    >
+      {description}
+    </ark.p>
+  );
 }
 
-export function Rating(props: Omit<RatingRow.RootProps, 'rating'>) {
-	const { rating } = useStoreStrictContext();
+export function Rating(props: Omit<RatingRow.RootProps, "rating">) {
+  const { rating } = useStoreStrictContext();
 
-	if(!rating)
-		return null;
+  if (!rating) return null;
 
-	return (
-		<RatingRow.Composed 
-			rating={rating} 
-			{...props}
-		/>
-	);
+  return <RatingRow.Composed rating={rating} {...props} />;
 }
 
 export function Composition() {
-	return (
-		<>
-			<ImageDesktop />
-			<Content>
-				<Title>
-					<ImageMobile />
-				</Title>
-				<Description />
-				<Rating />
-			</Content>
-		</>
-	);
+  return (
+    <>
+      <ImageDesktop />
+      <Content>
+        <Title>
+          <ImageMobile />
+        </Title>
+        <Description />
+        <Rating />
+      </Content>
+    </>
+  );
 }
 
 export function Composed(props: ComponentProps<typeof Root>) {
-	return (
-		<Root {...props}>
-			<Composition />
-		</Root>
-	);
+  return (
+    <Root {...props}>
+      <Composition />
+    </Root>
+  );
 }
--- src/pages/product/ui/ProductContent.tsx
+++ src/pages/product/ui/ProductContent.tsx
@@ -9,53 +9,52 @@ import { HTMLAttributes } from "react";
 import { cn } from "~/shared/lib/cn";
 import { Button } from "~/shared/ui/kit/button";
 
-export function ProductContent({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
-	const product = useProductStrictContext();
-	const images = product.galleryImages && product.previewImage ?
-		[product.previewImage, ...product.galleryImages]
-		: product.galleryImages
+export function ProductContent({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) {
+  const product = useProductStrictContext();
+  const images =
+    product.galleryImages && product.previewImage
+      ? [product.previewImage, ...product.galleryImages]
+      : product.galleryImages;
 
-	return (
-		<div {...props} className={cn("flex flex-col gap-6", className)}>
-			<div className="text-[1rem]/[1.3rem] text-black-60">
-				<span>by </span>
-				{product.storeUrl && (
-					<Link
-						className='text-accent-100'
-						href={getStorePathname(product.storeUrl)}
-					>
-						{product.storeUrl}
-					</Link>
-				)}
-			</div>
+  return (
+    <div {...props} className={cn("flex flex-col gap-6", className)}>
+      <div className="text-[1rem]/[1.3rem] text-black-60">
+        <span>by </span>
+        {product.storeUrl && (
+          <Link
+            className="text-accent-100"
+            href={getStorePathname(product.storeUrl)}
+          >
+            {product.storeUrl}
+          </Link>
+        )}
+      </div>
 
-			<Heading size='lg'>
-				{product.name}
-			</Heading>
+      <Heading size="lg">{product.name}</Heading>
 
-			<div className="flex flex-col gap-4 lg:gap-6 pb-8 border-b border-white/[.08]">
-				{images && (
-					<GalleryCarousel images={images} />
-				)}
-				<div className=" text-black-74 flex flex-col gap-[1.25rem]">
-					<p className="line-clamp-5 lg:line-clamp-none">
-						{Boolean(product.description) ? product.description : 'No description'}
-					</p>
+      <div className="flex flex-col gap-4 lg:gap-6 pb-8 border-b border-white/[.08]">
+        {images && <GalleryCarousel images={images} />}
+        <div className=" text-black-74 flex flex-col gap-[1.25rem]">
+          <p className="line-clamp-5 lg:line-clamp-none whitespace-pre-line">
+            {Boolean(product.description)
+              ? product.description
+              : "No description"}
+          </p>
 
-					<div className="flex flex-row gap-[1rem] flex-wrap">
-						{product.tagNames?.map(t => (
-							<Button
-								asChild
-								key={t} colorPalette="gray"
-							>
-								<Link href={`/marketplace/?tagNames=${encodeURIComponent(t)}`}>
-									{t}
-								</Link>
-							</Button>
-						))}
-					</div>
-				</div>
-			</div>
-		</div >
-	);
+          <div className="flex flex-row gap-[1rem] flex-wrap">
+            {product.tagNames?.map((t) => (
+              <Button asChild key={t} colorPalette="gray">
+                <Link href={`/marketplace/?tagNames=${encodeURIComponent(t)}`}>
+                  {t}
+                </Link>
+              </Button>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
