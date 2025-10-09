20e956bf9f07da9d8aeee03c8868d448d05cc7ca
MickaelNeves <nevesmickael8@gmail.com>
2025-10-09 17:33:49 +0100

    fix: metadata

--- src/app/[storeUrl]/page.tsx
+++ src/app/[storeUrl]/page.tsx
@@ -1,21 +1,18 @@
+import { Metadata } from "next";
 import { PageStore } from "~/pages/store";
 
 interface PageProps {
-	params: { storeUrl: string }
+  params: { storeUrl: string };
 }
 
 export const metadata: Metadata = {
   title: "Sella",
-  description: "Check out my shop on Sella - a crypto marketplace powered by escrow. No KYC. No Limits. Launch your free storefront in less than a minute!",
-  metadataBase: new URL(APP_BASE_URL),
+  description:
+    "Check out my shop on Sella - a crypto marketplace powered by escrow. No KYC. No Limits. Launch your free storefront in less than a minute!",
 };
 
 export default function Page({ params }: PageProps) {
-	return (
-		<PageStore
-			storeUrl={params.storeUrl}
-		/>
-	);
+  return <PageStore storeUrl={params.storeUrl} />;
 }
 
 export const revalidate = 0;
